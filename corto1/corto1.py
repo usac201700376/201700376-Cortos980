ba2bb088c63651cf02588ae9bc81aca960ceabf5
nojhuitzi <3064599650401@ingenieria.usac.edu.gt>
2020-06-09 12:29:48 -0600

    Agregando el archivo .py

---
+++ corto1/corto1.py
@@ -0,0 +1,22 @@
+def secuencia(n):
+    numeros=[n]
+    for i in range(1,n):
+        a=n
+        while(n>1):
+            if(a%2==0):
+                a//=2
+                numeros.append(a)
+                n=a
+            else:
+                a=3*a+1
+                numeros.append(a)
+                n=a
+            a=n
+    return numeros
+def sec(n):
+    for i in range(n+1):
+        finales=secuencia(i)
+        print(finales)
+    return finales
+a=int(input("ingrese un numero: "))
+sec(a)
